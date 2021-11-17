81aafbc0baa246b5e78af167a33a8f225a443a7d
Weiwei Wu <weiwei.wu@aspentech.com>
2021-11-17 16:30:48 -0500

    resolve picture capture issue within frame

--- controller/puppeteer/injection/eventRecorder.js
+++ controller/puppeteer/injection/eventRecorder.js
@@ -48,7 +48,7 @@ Object.keys(EVENTCONST).forEach(item => {
         let potentialMatch = getElementAttribute(event.target, BLUESTONE.bluestonePotentialMatchIndexes)
 
 
-        const position = event.target.getBoundingClientRect()
+        const position = getElementPos(event.target)
         const targetInnerText = event.target.innerText
         let parameter = null
         let command = item
@@ -109,7 +109,44 @@ Object.keys(EVENTCONST).forEach(item => {
         // console.log(JSON.stringify(event))
     })
 })
+/**
+ * get element location even though they are within iframe
+ * @param {HTMLElement} element 
+ * @returns {DOMRect}
+ */
+function getElementPos(element) {
+    /**@type {Array<DOMRect>} */
+    let iframePos = []
+    //build a queue of iframe
+    let currentFrameElement = window
+    let currentWindow = window
+    while (true) {
+        if (currentFrameElement.frameElement == null) {
+            break
+        }
+        currentFrameElement = currentFrameElement.frameElement
+        let leftPadding = window.getComputedStyle(currentFrameElement).getPropertyValue('padding-left').replace('px', '')
+        let topPadding = window.getComputedStyle(currentFrameElement).getPropertyValue('padding-top').replace('px', '')
+        if (currentFrameElement) {
+            let pos = currentFrameElement.getBoundingClientRect()
+            pos.x = pos.x + parseInt(leftPadding)
+            pos.y = pos.y + parseInt(topPadding)
 
+            iframePos.push(pos)
+        }
+
+    }
+    let position = element.getBoundingClientRect()
+    //calculate relative position of current element's iframe
+    console.log(iframePos)
+
+    iframePos.forEach(pos => {
+        console.log(pos)
+        position.x += pos.x
+        position.y += pos.y
+    })
+    return position
+}
 //XXX (RoadMap) Add a way to handle delete operation
 //draw rectangle and return the selector and inner text of element mouse hover on
 document.addEventListener('mouseover', async event => {
@@ -118,7 +155,7 @@ document.addEventListener('mouseover', async event => {
         const innerText = event.target.innerText
         let position = {}
         try {
-            position = event.target.getBoundingClientRect()
+            position = getElementPos(event.target)
         } catch (error) {
             console.log(error)
         }
