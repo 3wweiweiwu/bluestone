d346e08287f13c71a171d44349ce5068f809db28
Weiwei Wu <weiwei.wu@aspentech.com>
2022-01-08 10:27:37 -0500

    update input box behavior to enhance usability

--- public/javascript/redirectInput.js
+++ public/javascript/redirectInput.js
@@ -7,3 +7,13 @@ function redirectInputOnChange(event) {
     let decodedUri = (`?${queryKey}=${value}&${queryindex}=${index}`).replace(/#/g, '%23')
     window.location.replace(decodedUri)
 }
+
+function ajaxInputOnChange(event) {
+    let value = event.target.value
+    let queryKey = event.target.getAttribute('querykey')
+    let index = event.target.getAttribute('index')
+    let queryindex = event.target.getAttribute('queryIndex')
+    value = encodeURIComponent(value)
+    let decodedUri = (`?${queryKey}=${value}&${queryindex}=${index}`).replace(/#/g, '%23')
+    fetch(decodedUri, { method: 'get' })
+}
