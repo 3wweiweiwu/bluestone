e6ec5cd71d64bb93249c7385ecaf5d51c19d0486
Wix <longfeibisheng@gmail.com>
2022-02-04 22:32:52 -0500

    add sample function

--- controller/funcGen/sample.js
+++ controller/funcGen/sample.js
@@ -1,18 +1,44 @@
 const { Browser, Page, ElementHandle, Frame } = require('puppeteer-core')
 const { ElementSelector, VarSaver } = require('bluestone').types
-const bluestoneFunc = require('bluestone').func
+const bluestoneFunc = require('bluestone/ptLibrary/bluestone-func');
+const bluestoneType = require('bluestone/ptLibrary/class/index');
 const assert = require('assert')
 
 /**
- * Please add description
- * @param {Frame} frame The frame interface. 
- * @param {ElementSelector} element If this function be run against element we select
- * @param {VarSaver} vars If we will use local scope
- * @param {string} stringArg1 The string value required user input
- * * @param {number} numberArg The number value required user input
+ * This is a sample customized function. Feel free to add/modify/delete elements
+ * @param {Browser} browser The puppeteer browser class
+ * @param {Page} page The puppeteer page class
+ * @param {Frame} frame The puppeteer frame object. 
+ * @param {ElementSelector} element element this function will interact with. We can only have 1 element as input
+ * @param {VarSaver} vars The scope we will use
+ * @param {string} stringArg1 The string input
+ * * @param {number} numberArg The number input
  * @returns 
  */
-exports.sampleFunctionName = async function (frame, element, vars, stringArg1, numberArg) {
+exports.sampleFunctionName = async function (browser, page, frame, element, vars, stringArg1, numberArg) {
     assert.deepEqual(1, 2, 'Current function have not been implemented') //please remove this line after implmentation
     return true //please leave this line if you don't want to return value
+}
+
+
+exports.clearBrowserCache = class extends BluestoneFunc {
+    /**
+     * Clear browser cache
+     * @param {Page} page 
+     */
+    async func(page) {
+        try {
+            const client = await page.target().createCDPSession();
+            await client.send('Network.clearBrowserCookies');
+            await client.send('Network.clearBrowserCache');
+            await page.evaluate(() => { localStorage.clear() })
+        } catch (error) {
+
+        }
+
+        return true
+    }
+    constructor() {
+        super()
+    }
 }
