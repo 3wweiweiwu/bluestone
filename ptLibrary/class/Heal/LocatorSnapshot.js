754140b6bed1b2da872feb8a3fd879e4fc6fafb4
Wix <longfeibisheng@gmail.com>
2022-02-17 11:19:10 -0500

    initial commit for locator snapshot

---
+++ ptLibrary/class/Heal/LocatorSnapshot.js
@@ -0,0 +1,14 @@
+class LocatorRect {
+    constructor(x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.height = height
+        this.width = width
+    }
+}
+//This is used to create a locator snapshot in case script execution failed
+class LocatorSnapshot {
+    constructor() {
+
+    }
+}
