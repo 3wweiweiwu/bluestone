f20474f4f5828ee13ea7074006582e65389b3c5a
VELASQUD <daniel.velasquez@aspentech.com>
2022-06-09 08:44:01 -0700

    Add UnitTestin

--- test/workflow/testcase/spec.opeartion.js
+++ test/workflow/testcase/spec.opeartion.js
@@ -82,4 +82,143 @@ describe('Smoke Test - Operation Page', () => {
         assert.deepEqual(data.data.currentSelector, '5566', 'The selector value does not change')
 
     })
+
+    it('should change dropdown menu in the backend once selector value is changed in the bluestone console', async function () {
+        this.timeout(999990)
+        let happyPathPage = testConfig.testSite.page.happypath
+        await bluestoneBackend.startRecording(siteBackend.singlePageHappyPath)
+        await siteBackend.sendOperation('click', happyPathPage.header)
+        await siteBackend.sendOperation('change', happyPathPage.text_input_first_name, 'Wix')
+        await siteBackend.sendOperation('change', happyPathPage.text_input_last_name, 'Woo')
+        await siteBackend.sendOperation('click', happyPathPage.button_submit_form)
+        await new Promise(resolve => setTimeout(resolve, 500))
+        await siteBackend.callBluestoneTab(happyPathPage.header)
+
+        const browser = await puppeteer.launch(puppeteerSupport.config);
+        const page = await browser.newPage();
+        await bluestoneFunc.goto.func(page, bluestoneBackend.operationUrl)
+        
+        //Change operation Group using UI        
+        await bluestoneFunc.click.func(page, puppeteerSupport.Locator.Operation.DropDnMenu1)
+        await bluestoneFunc.click.func(page, puppeteerSupport.Locator.Operation.DropDnMenu1_Verify)
+        await new Promise(resolve => setTimeout(resolve, 500))
+        let userSelection = await bluestoneBackend.getUserSelection()
+        assert.deepEqual(userSelection['currentGroup'], 'assert', `The current operation was not added in the UI Operation`)
+
+        //Change operation using UI   
+        await bluestoneFunc.click.func(page, puppeteerSupport.Locator.Operation.DropDnMenu2)
+        await bluestoneFunc.click.func(page, puppeteerSupport.Locator.Operation.DropDnMenu1_TxtEqual)
+        await new Promise(resolve => setTimeout(resolve, 500))
+        userSelection = await bluestoneBackend.getUserSelection()
+        assert.deepEqual(userSelection['currentOperation'], 'testTextEqual', `The current group was not added in the UI Operation`)
+
+        //Changin Argument txt using UI
+        await bluestoneFunc.click.func(page, puppeteerSupport.Locator.Operation.argumentInput_0)
+        await bluestoneFunc.change.func(page, puppeteerSupport.Locator.Operation.argumentInput_0, 'Test')
+        await bluestoneFunc.keydown.func(page, 'Enter')
+        await new Promise(resolve => setTimeout(resolve, 500))
+        userSelection = await bluestoneBackend.getUserSelection()
+        assert.deepEqual(userSelection['currentArgument'][0], 'Test', `The Argument 0 must be updated`)
+
+        await bluestoneFunc.click.func(page, puppeteerSupport.Locator.Operation.btn_AddStep)
+        
+        await new Promise(resolve => setTimeout(resolve, 500))
+        let step = await bluestoneBackend.getSteps()
+
+        assert.deepEqual(step.data[step.data.length-1].command, 'testTextEqual', 'The testTextEqual step was not added')
+        assert.deepEqual(step.data[step.data.length-1].functionAst.params[2].pugType, 'Test', `The Argument 0 must be added in the step`)
+    })
+
+    it('UI Operation Spy Current Group should change accourding with the selected group', async function () {
+        this.timeout(99999)
+        let happyPathPage = testConfig.testSite.page.happypath
+        await bluestoneBackend.startRecording(siteBackend.singlePageHappyPath)
+        await siteBackend.sendOperation('click', happyPathPage.header)
+        await siteBackend.sendOperation('change', happyPathPage.text_input_first_name, 'Wix')
+        await siteBackend.sendOperation('change', happyPathPage.text_input_last_name, 'Woo')
+        await siteBackend.sendOperation('click', happyPathPage.button_submit_form)
+        await new Promise(resolve => setTimeout(resolve, 500))
+        await siteBackend.callBluestoneTab(happyPathPage.header)
+
+        const browser = await puppeteer.launch(puppeteerSupport.config);
+        const page = await browser.newPage();
+        await bluestoneFunc.goto.func(page, bluestoneBackend.operationUrl)
+        //await bluestoneFunc.change.func(page, puppeteerSupport.Locator.Operation.DropDnMenu1, 'Verify')
+        
+        let operationGroup = await bluestoneBackend.getOperationGroup()
+        let operationLst = Object.keys(operationGroup)
+        for (let i = 0; i< operationLst.length; i++)
+        {
+            let group = operationLst[i]
+            await siteBackend.sendSpy('currentGroup',group)
+            await new Promise(resolve => setTimeout(resolve, 500))
+            let userSelection = await bluestoneBackend.getUserSelection()
+            assert.deepEqual(userSelection['currentGroup'], group, `The current group was not added in the UI Operation`)
+        }
+    })
+
+    it('UI Operation Spy Current Operation should change accourding with the selected operation', async function () {
+        this.timeout(99999)
+        let happyPathPage = testConfig.testSite.page.happypath
+        await bluestoneBackend.startRecording(siteBackend.singlePageHappyPath)
+        await siteBackend.sendOperation('click', happyPathPage.header)
+        await siteBackend.sendOperation('change', happyPathPage.text_input_first_name, 'Wix')
+        await siteBackend.sendOperation('change', happyPathPage.text_input_last_name, 'Woo')
+        await siteBackend.sendOperation('click', happyPathPage.button_submit_form)
+        await new Promise(resolve => setTimeout(resolve, 500))
+        await siteBackend.callBluestoneTab(happyPathPage.header)
+
+        const browser = await puppeteer.launch(puppeteerSupport.config);
+        const page = await browser.newPage();
+        await bluestoneFunc.goto.func(page, bluestoneBackend.operationUrl)
+        //await bluestoneFunc.change.func(page, puppeteerSupport.Locator.Operation.DropDnMenu1, 'Verify')
+        
+        let operationGroup = await bluestoneBackend.getOperationGroup()
+        let operationLst = Object.keys(operationGroup)
+
+        for (let i = 0; i< operationLst.length; i++)
+        {
+            let group = operationLst[i]
+            await siteBackend.sendSpy('currentGroup',group)
+            for (let j = 0; j<operationGroup[group].operations.length; j++)
+            {
+                let operation = operationGroup[group].operations[j].name
+                await siteBackend.sendSpy('currentOperation',operation)
+                await new Promise(resolve => setTimeout(resolve, 500))
+                let userSelection = await bluestoneBackend.getUserSelection()
+                assert.deepEqual(userSelection['currentOperation'], operation, `The current operation was not added in the UI Operation`)
+            }
+        }
+    })
+
+    it('UI New options', async function () {
+        this.timeout(99999)
+        let happyPathPage = testConfig.testSite.page.happypath
+        await bluestoneBackend.startRecording(siteBackend.singlePageHappyPath)
+        await siteBackend.sendOperation('click', happyPathPage.header)
+        await siteBackend.sendOperation('change', happyPathPage.text_input_first_name, 'Wix')
+        await siteBackend.sendOperation('change', happyPathPage.text_input_last_name, 'Woo')
+        await siteBackend.sendOperation('click', happyPathPage.button_submit_form)
+        await new Promise(resolve => setTimeout(resolve, 500))
+        await siteBackend.callBluestoneTab(happyPathPage.header)
+
+        const browser = await puppeteer.launch(puppeteerSupport.config);
+        const page = await browser.newPage();
+        await bluestoneFunc.goto.func(page, bluestoneBackend.operationUrl)
+        //await bluestoneFunc.change.func(page, puppeteerSupport.Locator.Operation.DropDnMenu1, 'Verify')
+        
+        let operationGroup = await bluestoneBackend.getOperationGroup()
+        let operationLst = Object.keys(operationGroup)
+
+
+        let group = operationLst[0]
+        await siteBackend.sendSpy('currentGroup',group)
+        await new Promise(resolve => setTimeout(resolve, 500))
+
+
+        let operation = operationGroup[group].operations[0].name
+        await siteBackend.sendSpy('currentOperation',operation)
+        await new Promise(resolve => setTimeout(resolve, 500))
+
+    })
 })
